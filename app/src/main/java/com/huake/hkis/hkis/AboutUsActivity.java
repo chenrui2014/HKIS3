5bd5a38ef344f68b9c1680f5d77f31e5f77c9d5e
chenrui2014 <chenruixiang2009@163.com>
2017-06-13 17:50:45 +0800

    上架信息，出库汇总，title

--- app/src/main/java/com/huake/hkis/hkis/AboutUsActivity.java
+++ app/src/main/java/com/huake/hkis/hkis/AboutUsActivity.java
@@ -1,17 +1,63 @@
 package com.huake.hkis.hkis;
 
+import android.arch.lifecycle.LifecycleRegistry;
+import android.arch.lifecycle.LifecycleRegistryOwner;
+import android.arch.lifecycle.LiveData;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.huake.hkis.hkis.dagger.AppModule;
+import com.huake.hkis.hkis.model.AboutUs;
+import com.huake.hkis.hkis.repository.HKISRepository;
 
 /**
  * Created by chen on 2017/6/13.
  */
 
-public class AboutUsActivity extends AppCompatActivity {
+public class AboutUsActivity extends AppCompatActivity implements LifecycleRegistryOwner {
+
+    private final LifecycleRegistry lifecycleRegistry = new LifecycleRegistry(this);
+
+    private HKISRepository hkisRep;
+
+    private TextView projectInfoTv;
+
+    private TextView wechatServerTv;
+    private TextView officialSiteTv;
+    private TextView offocoalMailTv;
+    private TextView officialTelTv;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_about);
+        AppModule appModule = new AppModule(getApplication());
+        hkisRep = appModule.providesHKISRepository(appModule.providesHKISApi());
+
+        projectInfoTv = (TextView) findViewById(R.id.about_about);
+        wechatServerTv = (TextView) findViewById(R.id.about_wx);
+        officialSiteTv = (TextView) findViewById(R.id.about_offic);
+        offocoalMailTv = (TextView) findViewById(R.id.about_com);
+        officialTelTv = (TextView) findViewById(R.id.about_phone);
+        initData();
+    }
+
+    private void initData(){
+        LiveData<AboutUs> aboutUsData = hkisRep.aboutUs();
+        aboutUsData.observe(this,aboutUs ->{
+
+            projectInfoTv.setText(aboutUs.getProjectInfo());
+            wechatServerTv.setText(aboutUs.getWebchatServer());
+            officialSiteTv.setText(aboutUs.getOfficialSite());
+            offocoalMailTv.setText(aboutUs.getOffocoalEmail());
+            officialTelTv.setText(aboutUs.getOfficialTel());
+        });
+    }
+
+    @Override
+    public LifecycleRegistry getLifecycle() {
+        return lifecycleRegistry;
     }
 }
